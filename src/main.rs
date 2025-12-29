deb0ef3a96009bc8312bf0b0925077c033a89f5c
MicroRay620 <microray620@gmail.com>
2025-12-28 21:48:06 -0500

    Added more documentation to break down the directories that will be included

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,16 @@
  * * In $HOME/.config/mulbrowser/keybinds.lua 
  * * * Uses the Neovim way of setting keybinds `vim.keymap.set("MODE", "KEY", "COMMAND")`
  */
+/*
+ * Directories
+ * src/ is for the core functionality of the browser
+ * src/core-plugins is for the core mulbrowser plugins. 
+ * * This contains all the vim definitions
+ * * This is written in Lua 
+ * src/ui is for the UI of mulbrowser. 
+ * * This is written in Rust with the Leptos and Stylus crates 
+ */
+
 fn main() {
     println!("Hello, world!");
 }
